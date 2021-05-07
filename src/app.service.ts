0ec331a0b1ac85883054e7497b90fb05d71bb721
Getulio Magela Silva <getuliomagela@outlook.com>
2021-05-07 11:35:01 -0300

    feat: test cd again

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!\nHello again!';
+    return `${new Date()} - Hello World!\nHello again!`;
   }
 }
