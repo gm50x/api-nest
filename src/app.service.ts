1252c530b30cc6f580219c842db6073997b1a54b
Getulio Magela Silva <getuliomagela@outlook.com>
2021-05-07 14:41:35 -0300

    feat: added tests back to the pipeline and rolled back app.services

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `${new Date()} - Hello World!\nHello again! Fooo Bar`;
+    return `Hello World!`;
   }
 }
